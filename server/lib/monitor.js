3af05746ca70dfd4ab796f750a39951a4c2c033c
olln3 <johnnyyellow@gmail.com>
2016-04-15 10:03:59 +0800

    ESlint monitor

--- server/lib/monitor.js
+++ server/lib/monitor.js
@@ -3,11 +3,10 @@ import mongoose from 'mongoose';
 import * as executor from './executor';
 import Promise from 'bluebird';
 
-let monitor = (function() {
-	let api = {},
-		executors = [],
-		transporter;
-	const monitorSchema = mongoose.Schema({
+let monitor = ((function () {
+	let api = {};
+	let	transporter;
+	const monitorSchema = new mongoose.Schema({ // eslint-disable-line new-cap
 		nuser: {
 			type: String,
 			index: true,
@@ -24,29 +23,29 @@ let monitor = (function() {
 		notifiers: {
 			emails: [String],
 			wechats: [String],
-		}
+		},
 	});
-	monitorSchema.statics.findAndModifyAsync = function(query, sort, doc, options){
-		let _this = this;
-		return new Promise(function(resolve, reject){
-			_this.collection.findAndModify(query, sort, doc, options, (err, model) => {
-				if(err)
+	monitorSchema.statics.findAndModifyAsync = function (query, sort, doc, options) {
+		let m = this;
+		return new Promise((resolve, reject) => {
+			m.collection.findAndModify(query, sort, doc, options, (err, model) => {
+				if(err) {
 					reject(err);
-				else{
+				}else{
 					resolve(model);
 				}
 			});
 		});
 	};
-	var MonitorModel = mongoose.model('Monitor', monitorSchema);
+	let MonitorModel = mongoose.model('Monitor', monitorSchema);
 
 	function createSMTP() {
 		if (!transporter) {
 			transporter = nodemailer.createTransport({
-				host : 'smtp.163.com',
-				port : 465,
+				host: 'smtp.163.com',
+				port: 465,
 				secure: true,
-				auth : {
+				auth: {
 					user: 'xhjappadmin@163.com',
 					pass: 'devc0re4',
 				},
@@ -64,13 +63,13 @@ let monitor = (function() {
 		let mailOptions = {
 			from: 'xhjappadmin@163.com', // sender address
 			to: notifiers.join(', '),
-			subject: blockname || url + ' 有更新',
-			text: '新内容:\n' + value, // plaintext body
+			subject: `${blockname || url}有更新`,
+			text: `新内容:\n${value}`, // plaintext body
 		};
 		createSMTP();
 		//send mail with defined transport object
 		let info = await transporter.sendMailAsync(mailOptions).catch((err) => {throw err;});
-		console.log('Message sent: ' + info.response);
+		console.log(`Message sent: ${info.response}`);
 		return info;
 	};
 	// Return the monitor
@@ -78,44 +77,44 @@ let monitor = (function() {
 		if (!global.executors) global.executors = {};
 		return global.executors[id];
 	}
-	/*	
+	/*
 	 * monitor: monitorSchema like
 	 *	callback: error
 	 */
-	api.restartMonitor = (monitor) => {
-		api.stopMonitor(monitor);
-		api.startMonitor(monitor);
-	}
+	api.restartMonitor = (m) => {
+		api.stopMonitor(m);
+		api.startMonitor(m);
+	};
 
-	api.stopMonitor = (monitor) => {
-		let id = monitor.url + monitor.jqpath;
-		let exe = checkMonitor(id)
+	api.stopMonitor = ({url, jqpath}) => {
+		let id = url + jqpath;
+		let exe = checkMonitor(id);
 		if (exe) {
 			exe.clear();
 		}
-	}
+	};
 
-	api.startMonitor = async ({url, jqpath, notifiers, blockname, value}) => {
+	api.startMonitor = async ({url, jqpath, notifiers, blockname}) => {
 		let id = url + jqpath;
-		let exe = checkMonitor(id)
+		let exe = checkMonitor(id);
 		if (!exe) {
-			console.log(`create new monitor with ${executor}`);	
-			exe = executor.runInSchedule(async (value)=> {
-				console.log(url + ' has update!');
-				if(notifiers.emails){
+			console.log(`create new monitor with ${executor}`);
+			exe = executor.runInSchedule(async (value) => {
+				console.log(`${url} has update!`);
+				if(notifiers.emails) {
 					await api.sendEmail({
-						notifiers: notifiers.emails, 
-						url, value, blockname
+						notifiers: notifiers.emails,
+						url, value, blockname,
 					});
-					console.log("Email sent to " + notifiers.emails + "\nurl:" + url);
+					console.log(`Email sent to ${notifiers.emails}\nurl: ${url}`);
 				}
-				if(notifiers.wechats){
+				if(notifiers.wechats) {
 					//TODO
 				}
-			},{url, jqpath});					
-		};
+			}, {url, jqpath});
+		}
 		global.executors[id] = exe;
-	}
+	};
 
 	// function compareNotifiers(a, b) {
 	// 	var result = true;
@@ -144,27 +143,25 @@ let monitor = (function() {
 	// 	return result;
 	// }
 
-	/*	
+	/*
 	 * opts: monitorSchema like
 	 *	callback: error
 	 */
-	api.addOrUpdateMonitor = async (opts, callback) => {
+	api.addOrUpdateMonitor = async ({url, jqpath, nuser, notifiers, blockname}) => {
 		let m = await MonitorModel.findAndModifyAsync({
-			url: opts.url,
-			jqpath: opts.jqpath,
-			nuser: opts.nuser
+			url, jqpath, nuser,
 		}, [], {
 			$setOnInsert: {
-				notifiers: opts.notifiers,
-				blockname: opts.blockname
-			}
+				notifiers,
+				blockname,
+			},
 		}, {
 			new: true,
-			upsert: true
+			upsert: true,
 		}).catch((err) => {throw err;});
-		console.log('updated ' + m.value._id);
+		console.log(`updated ${m.value._id}`);
 		api.restartMonitor(m.value);
-		
+
 		// MonitorModel.find({
 		// 	url: opts.url,
 		// 	jqpath: opts.jqpath,
@@ -190,15 +187,14 @@ let monitor = (function() {
 		// });
 	};
 
-	api.findMonitors = function(opts, callback) {
+	api.findMonitors = function () {
 
 	};
 
-	api.deleteMonitors = function(opts, callback) {
+	api.deleteMonitors = function () {
 
 	};
-
 
 	return api;
-})();
+})());
 export default monitor;
